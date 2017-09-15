Fix bound computation

qball/tools/bounds.py
@@ -17,37 +17,30 @@ def compute_bounds(b_sph, data, c=1.0):
     loglog_data = np.log(-np.log(data_clipped))
     f[:] = loglog_data.reshape(-1, l_labels).T
 
-    if len(data.shape) == 2:
-        maskdata, mask = median_otsu(data[:,None,None,:], 3, 1, True,
-                                     vol_idx=range(10, 50), dilate=2)
-    elif len(data.shape) == 3:
-        maskdata, mask = median_otsu(data[:,:,None,:], 3, 1, True,
-                                     vol_idx=range(10, 50), dilate=2)
-    elif len(data.shape) == 4:
-        maskdata, mask = median_otsu(data[:,:,:,:], 3, 1, True,
-                                     vol_idx=range(10, 50), dilate=2)
+    3d_data = data[(slice(None),)*d_image + (None,)*(3-d_image) + (slice(None),)]
+    maskdata, mask = median_otsu(3d_data)
 
     n_samples = np.sum(np.logical_not(mask))
     print('n_samples = ', n_samples)
-    
+
     samples = data[np.logical_not(mask.reshape(imagedims))]
     samples -= samples.min(0)
 
     assert(samples.shape == (n_samples,l_labels))
-    
+
 #    print(samples[:,0])
-    
+
     noise_l = np.percentile(samples, c/2, axis=0)
     noise_u = np.percentile(samples, 1.0-c/2, axis=0)
-    
+
     print('Noise in (',noise_l[0],',',noise_u[0],')')
-    
+
 #    print('Data')
 #    print(data[10,10,:])
-    
+
     data_l = data - noise_u
     data_u = data - noise_l
-    
+
     assert((data_l <= data_u).all())
 
     data_l_clipped = np.clip(data_l, np.spacing(1), 1-np.spacing(1))
@@ -55,9 +48,9 @@ def compute_bounds(b_sph, data, c=1.0):
 
     fl[:] = np.log(-np.log(data_u_clipped)).reshape(-1, l_labels).T
     fu[:] = np.log(-np.log(data_l_clipped)).reshape(-1, l_labels).T
-    
+
     assert(fl.shape == (l_labels,n_image))
-    
+
 #    for i in range(n_image):
 #        for k in range(l_labels):
 #            if f[k,i] > 0: